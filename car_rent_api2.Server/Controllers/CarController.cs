ecce652ea0934fbb6eca6f7dc462f7d2016930b9
pietraldo <pietrzakjakub4321@gmail.com>
2024-11-12 23:40:26 +0100

    from and list of cars

--- car_rent_api2.Server/Controllers/CarController.cs
+++ car_rent_api2.Server/Controllers/CarController.cs
@@ -8,22 +8,25 @@ namespace car_rent_api2.Server.Controllers
     [ApiController]
     public class CarController : ControllerBase
     {
-        private List<Car> cars = new List<Car>();
+        private static List<Car> cars = new List<Car>();
 
         public CarController()
         {
-            cars.Add(new Car() { Id = 1, Brand = "Toyota", Model = "Corolla", Year = 2019, Photo = "https://www.cstatic-images.com/car-pictures/xl/USC90TOC021A021001.png", IsRented = false, Price = 100, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
-            cars.Add(new Car() { Id = 2, Brand = "Toyota", Model = "Yaris", Year = 2018, Photo = "https://www.cstatic-images.com/car-pictures/xl/USC90TOC021A021001.png", IsRented = false, Price = 90, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
-            cars.Add(new Car() { Id = 3, Brand = "Toyota", Model = "Auris", Year = 2017, Photo = "https://www.cstatic-images.com/car-pictures/xl/USC90TOC021A021001.png", IsRented = false, Price = 80, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
-            cars.Add(new Car() { Id = 4, Brand = "Toyota", Model = "Avensis", Year = 2016, Photo = "https://www.cstatic-images.com/car-pictures/xl/USC90TOC021A021001.png", IsRented = false, Price = 70, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
-            cars.Add(new Car() { Id = 5, Brand = "Toyota", Model = "Prius", Year = 2015, Photo = "https://www.cstatic-images.com/car-pictures/xl/USC90TOC021A021001.png", IsRented = false, Price = 60, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
+            if(cars.Count == 0)
+            {
+                cars.Add(new Car() { Id = 1, Brand = "Toyota", Model = "Corolla", Year = 2019, Photo = "https://image.api.playstation.com/vulcan/img/rnd/202011/0204/jvMomz0n9Be5mRKU8VP9Jl2A.png?w=440", IsRented = false, Price = 100, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
+                cars.Add(new Car() { Id = 2, Brand = "Toyota", Model = "Yaris", Year = 2018, Photo = "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-1fndzcd_41017374.jpeg?region=0,0,600,600", IsRented = false, Price = 90, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
+                cars.Add(new Car() { Id = 3, Brand = "Toyota", Model = "Auris", Year = 2017, Photo = "https://m.atcdn.co.uk/ect/media/%7Bresize%7D/4b14ab0c7868451baf5912779f112f40.jpg", IsRented = false, Price = 80, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
+                cars.Add(new Car() { Id = 4, Brand = "Toyota", Model = "Avensis", Year = 2016, Photo = "https://devil-cars.pl/storage/images/Tko1o17zh4AjbSQOUJhleUaysof12M7CfFMo8itK.jpg", IsRented = false, Price = 70, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
+                cars.Add(new Car() { Id = 5, Brand = "Toyota", Model = "Prius", Year = 2015, Photo = "https://storage.googleapis.com/pod_public/1300/121015.jpg", IsRented = false, Price = 60, location = new Location() { Latitude = 52.2297700, Longitude = 21.0117800 } });
+            }
         }
 
         // GET: api/<CarController>
         [HttpGet]
         public IEnumerable<Car> Get()
         {
-            Thread.Sleep(2000);
+            
             return cars;
         }
 
@@ -36,9 +39,10 @@ namespace car_rent_api2.Server.Controllers
 
         // POST api/<CarController>
         [HttpPost]
-        public void Post([FromBody] Car car)
+        public ActionResult<Car> Post([FromBody] Car car)
         {
             cars.Add(car);
+            return CreatedAtAction(nameof(Post), new { id = car.Id }, car);
         }
 
         // PUT api/<CarController>/5
--- car_rent_api2.client/src/App.jsx
+++ car_rent_api2.client/src/App.jsx
@@ -3,35 +3,146 @@ import './App.css';
 
 function App()
 {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
+    const [editingCar, setEditingCar] = useState(null); 
+
+    const fetchCarList = async () =>
+    {
+        const response = await fetch('api/Car');
+        if (response.ok)
+        {
+            const jsonData = await response.json();
+            setData(jsonData);
+        }
+    };
 
     useEffect(() =>
     {
-        const fetchData = async () =>
-        {
-
-            const response = await fetch('api/Car');
-
-            if (!response.ok) return;
-            const jsonData = await response.json();
-            setData(jsonData);
-
-            console.log(response.body);
-
-        };
-
-
-        fetchData();
+        fetchCarList();
     }, []);
 
 
+    const handleEdit = (car) =>
+    {
+        setEditingCar(car); 
+    };
+
+
+    const handleCarAddedOrEdited = () =>
+    {
+        fetchCarList(); 
+        setEditingCar(null); 
+    };
+
     return (
-        <div>
-            {!data ? (
-                'Loading...'
-            ) :
-                <pre>{JSON.stringify(data, null, 2)}</pre>
+        <div className="container">
+            <div className="section">
+                <h1>{editingCar ? 'Edit Car' : 'Add Car'}</h1>
+                <Formularz
+                    onCarAddedOrEdited={handleCarAddedOrEdited}
+                    editingCar={editingCar}
+                />
+            </div>
+            <div className="section">
+                <h1>Car List</h1>
+                <input type="text" placeholder="Search..." className="searchInput" />
+                <div className="carList">
+                    {data.map((car) => (
+                        <CarItem key={car.id} car={car} onEdit={() => handleEdit(car)} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
+function Formularz({ onCarAddedOrEdited, editingCar })
+{
+    const [formData, setFormData] = useState({
+        brand: '',
+        model: '',
+        year: '',
+        price: '',
+        location: ''
+    });
+
+    useEffect(() =>
+    {
+        if (editingCar)
+        {
+            setFormData(editingCar); // Populate form with editing car data
+        }
+    }, [editingCar]);
+
+    const handleInputChange = (e) =>
+    {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleFormSubmit = async (e) =>
+    {
+        e.preventDefault();
+
+        if (editingCar)
+        {
+            // Update car (PUT request)
+            const response = await fetch(`api/Car/${editingCar.id}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok)
+            {
+                onCarAddedOrEdited(); 
             }
+        } else
+        {
+            const response = await fetch('api/Car', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok)
+            {
+                onCarAddedOrEdited(); 
+                setFormData({ brand: '', model: '', year: '', price: '', location: '' }); 
+            }
+        }
+    };
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <input type="text" name="brand" placeholder="Brand" value={formData.brand} onChange={handleInputChange} />
+            <input type="text" name="model" placeholder="Model" value={formData.model} onChange={handleInputChange} />
+            <input type="number" name="year" placeholder="Year" value={formData.year} onChange={handleInputChange} />
+            <input type="number" name="price" placeholder="Price" value={formData.price} onChange={handleInputChange} />
+            <input type="file" name="photo" placeholder="Photo" />
+            <input type="text" name="location" placeholder="Location" value={formData.location} onChange={handleInputChange} />
+            <button type="submit">{editingCar ? 'Edit' : 'Add'}</button>
+        </form>
+    );
+}
+
+function CarItem({ car, onEdit })
+{
+    return (
+        <div className="carItem">
+            <img src={car.photo} alt="Car" className="carImage" />
+            <div className="carDetails">
+                <h2 className="carBrand">{car.brand}</h2>
+                <p className="carModel">{car.model}</p>
+                <p className="carYear">{car.year}</p>
+                <p className="carPrice">{car.price}</p>
+            </div>
+            <div className="carActions">
+                <button className="editButton" onClick={onEdit}>Edit</button>
+                <button className="deleteButton">Delete</button>
+            </div>
         </div>
     );
 }
