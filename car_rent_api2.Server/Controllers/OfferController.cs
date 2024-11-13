ad4c6f4430a26cdbd1d7cd5275ca179706c13813
pietraldo <pietrzakjakub4321@gmail.com>
2024-11-13 12:30:32 +0100

    a

---
+++ car_rent_api2.Server/Controllers/OfferController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace car_rent_api2.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+        private readonly CarRentDbContext _context;
+
+        public OfferController(CarRentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Offer
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            List<string> offers = new List<string>();
+            offers.Add("Offer 1");
+            offers.Add("Offer 2");
+            string a = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            if(a!=null)
+            {
+                offers.Add(a.Substring(0, 10));
+            }
+
+            
+            return offers;
+        }
+
+        
+    }
+}
