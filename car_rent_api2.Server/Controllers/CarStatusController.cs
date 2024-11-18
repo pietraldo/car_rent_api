59a9eec2d6b08fb58e087fd3c96377ea0b0b5af1
pietraldo <pietrzakjakub4321@gmail.com>
2024-11-18 20:55:02 +0100

    aviable cars first view

---
+++ car_rent_api2.Server/Controllers/CarStatusController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace car_rent_api2.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarStatusController : ControllerBase
+    {
+        private readonly CarRentDbContext _context;
+
+        public CarStatusController(CarRentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Car
+        [HttpGet("status/{status}")]
+        public async Task<ActionResult<IEnumerable<Car>>> Get(string status)
+        {
+            return await _context.Cars.Where(c=>c.Status==status).ToListAsync();
+        }
+
+        
+    }
+}
--- car_rent_api2.client/src/pages/AviableCarsView.jsx
+++ car_rent_api2.client/src/pages/AviableCarsView.jsx
@@ -1,4 +1,68 @@
-export default function AviableCarsView()
+import { useState, useEffect } from "react";
+
+function AviableCarsView()
 {
-    return <h1>Welcome to the Home Page</h1>;
+    const [carsToDisplay, setCarsToDisplay] = useState([]);
+
+    const fetchCarList = async () =>
+    {
+        const response = await fetch('api/Car');
+        if (response.ok)
+        {
+            const jsonData = await response.json();
+            setCarsToDisplay(jsonData);
+        }
+        filterCars();
+    };
+
+    const handleSearchResults = (e) =>
+    {
+
+    }
+
+    useEffect(() =>
+    {
+        fetchCarList();
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="section">
+                <h1>Car List</h1>
+                <input type="text" placeholder="Search..." className="searchInput" onChange={handleSearchResults} />
+                <select className="searchSelect" onChange={handleSearchResults}>
+                    <option>Brand</option>
+                    <option>Model</option>
+                </select>
+                <div className="carList">
+                    {carsToDisplay.map((car) => (
+                        <CarItem
+                            key={car.id}
+                            car={car}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
+
+function CarItem({ car })
+{
+    return (
+        <div className="carItem">
+            <img src={car.photo} alt="Car" className="carImage" />
+            <div className="carDetails">
+                <h2 className="carBrand">{car.brand}</h2>
+                <p className="carModel">{car.model}</p>
+                <p className="carYear">{car.year}</p>
+                <p className="carPrice">{car.price}</p>
+            </div>
+            <div className="carActions">
+                <button className="editButton">{car.status}</button>
+            </div>
+        </div>
+    );
+}
+
+export default AviableCarsView;
