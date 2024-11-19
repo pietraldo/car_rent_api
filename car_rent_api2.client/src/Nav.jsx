d5d45b6022960503a88532e1696dd9dc805ef135
Popke <popek069@gmail.com>
2024-11-19 23:18:19 +0100

    add login with google button

--- car_rent_api2.client/src/Nav.jsx
+++ car_rent_api2.client/src/Nav.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { FaHome, FaCar } from "react-icons/fa"; 
+import {Link} from "react-router-dom";
+import {FaHome, FaCar} from "react-icons/fa";
 import "./css/Nav.css";
 
-function Nav()
-{
+function Nav() {
     return (
         <nav className="modern-navbar">
             <div className="navbar-brand">CarRental</div>
             <ul className="navbar-links">
                 <li>
                     <Link to="/" className="navbar-link">
-                        <FaHome className="icon" /> Home
+                        <FaHome className="icon"/> Home
                     </Link>
                 </li>
                 <li>
                     <Link to="/aviablecarview" className="navbar-link">
-                        <FaCar className="icon" /> Available Cars
+                        <FaCar className="icon"/> Available Cars
                     </Link>
                 </li>
                 <li>
                     <Link to="/addeditcar" className="navbar-link">
-                        <FaCar className="icon" /> Add/Edit Car
+                        <FaCar className="icon"/> Add/Edit Car
                     </Link>
                 </li>
+                <li>
+                    <a href="/api/Identity/google-login" className="navbar-link" data-method="post">Login with
+                        Google</a></li>
             </ul>
         </nav>
     );
