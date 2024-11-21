049d48f6f728002c3a35527b6d0d20be56e41c99
pietraldo <pietrzakjakub4321@gmail.com>
2024-11-21 11:46:02 +0100

    generating offer

--- car_rent_api2.Server/Controllers/OfferController.cs
+++ car_rent_api2.Server/Controllers/OfferController.cs
@@ -20,17 +20,38 @@ namespace car_rent_api2.Server.Controllers
 
         // GET: api/Offer
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<string>>> Get()
+        public async Task<ActionResult<IEnumerable<string>>> Get(DateTime startDate, DateTime endDate, string brand="", string model="")
         {
-            List<string> offers = new List<string>();
-            offers.Add("Offer 1");
-            offers.Add("Offer 2");
-            string a = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
-                offers.Add((string)de.Key);
+            if (startDate >= endDate)
+            {
+                return BadRequest("Invalid date range. Start date must be earlier than end date.");
+            }
 
+            var carsQuery = _context.Cars
+                .Where(c => c.Status == "available")
+                .Include(c => c.Details)
+                .Include(c => c.Services).AsQueryable();
 
-            return offers;
+            // Filter by brand if specified
+            if (!string.IsNullOrEmpty(brand))
+            {
+                carsQuery = carsQuery.Where(c => c.Brand == brand);
+            }
+
+            // Filter by model if specified
+            if (!string.IsNullOrEmpty(model))
+            {
+                carsQuery = carsQuery.Where(c => c.Model == model);
+            }
+
+            
+            var cars = await carsQuery
+                .Where(c => !_context.Rents.Any(r =>
+                    r.CarId == c.Id &&
+                    (r.StartDate < endDate && r.EndDate > startDate))) 
+                .ToListAsync();
+
+            return Ok(cars);
         }
 
         
