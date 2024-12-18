afa5a213ef7a8ad3212c7d2687d1140d0f671d49
pietraldo <pietrzakjakub4321@gmail.com>
2024-12-18 15:32:22 +0100

    rent controller

---
+++ car_rent_api2.Server/Controllers/RentController.cs
@@ -0,0 +1,46 @@
+﻿using car_rent_api2.Server.Models;
+using car_rent_api2.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace car_rent_api2.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentController : ControllerBase
+    {
+        private readonly CarRentDbContext _context;
+
+        public RentController(CarRentDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getrents")]
+        public async Task<ActionResult<IEnumerable<Rent>>> GetRents()
+        {
+            return await _context.Rents.ToListAsync();
+        }
+
+        [HttpPost("uploadnote")]
+        public async Task<ActionResult> UploadNote(int id, string note, string linkToPhotos)
+        {
+            var rent = await _context.Rents.FirstOrDefaultAsync(r=>r.Id==id);
+            if (rent == null)
+            {
+                return NotFound();
+            }
+
+            rent.Notes = note;
+            rent.LinkToPhotos = linkToPhotos;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
